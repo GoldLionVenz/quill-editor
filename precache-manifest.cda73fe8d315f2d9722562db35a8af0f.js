85e7147b2e85d9e761db412e5d62d50aee471c8f
Cesar Olivero <oliverogero@gmail.com>
2020-10-02 13:44:52 -0400

    Updates

---
+++ precache-manifest.cda73fe8d315f2d9722562db35a8af0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec064b84613f58c37f894a70d6456e47",
+    "url": "/quill-editor/index.html"
+  },
+  {
+    "revision": "c1eb17324ae3295c4b3c",
+    "url": "/quill-editor/static/css/main.21fcaf9c.chunk.css"
+  },
+  {
+    "revision": "eab5f15a45877c045745",
+    "url": "/quill-editor/static/js/2.635075f2.chunk.js"
+  },
+  {
+    "revision": "6a3fc314c98f7e28971c1ca8b88f30af",
+    "url": "/quill-editor/static/js/2.635075f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1eb17324ae3295c4b3c",
+    "url": "/quill-editor/static/js/main.2c871dcb.chunk.js"
+  },
+  {
+    "revision": "f0151016c61abd6bfb40",
+    "url": "/quill-editor/static/js/runtime-main.9da12a13.js"
+  }
+]);
